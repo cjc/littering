cfcf737f4a72d063ecef89c2b610748767f72451
Cameron <cjc@cjc.id.au>
2011-05-17 09:03:55 +1000

    Fold up littering API to handle (), (words), (lines) with default numbering behaviour, prep for generic (sfunc, ffunc) overload

--- littering.js
+++ littering.js
@@ -1,41 +1,57 @@
 !function($) {
-  var injector = function(formatter,t,splitter,klass,after) {
+  var injector = function(formatter,t,splitter) {
       var a = $(t).text().split(splitter);
       if (a.length) {
         var inject = '';
         $(a).each(function(item,i) {
-          inject += formatter(item, i, klass, after);
+          inject += formatter(item, i);
         });
-        $(t).empty().append(inject);
+        $(t).empty().html(inject);
       }
   };
   var formatters = {
-    numbered : function(item, i, klass, after) {
-      return '<span class="'+klass+(i+1)+'">'+item+'</span>'+after;
+    numbered : function(klass, after) {
+      return function(item, i) {
+        return '<span class="'+klass+(i+1)+'">'+item+'</span>'+after;
+      }
     }
   };
   var splitters = {
-    words: function(context){
-      context.each(function(item){injector(formatters.numbered, item,' ','word',' ')});
+    words: function(context, formatter){
+      context.each(function(item){injector(formatter, item,' ')});
     },
-    chars: function(context){
-      context.each(function(item){injector(formatters.numbered, item,'','char','')});
+    chars: function(context, formatter){
+      context.each(function(item){injector(formatter, item,'')});
     },
-    lines: function(context){
+    lines: function(context, formatter){
       var r = "eefec303079ad17405c889e092e105b0";
       context.each(function(item){
         $(item).children("br").after(r).remove();
-        injector(formatters.numbered, item,r,'line','');
+        injector(formatter, item,r);
       });
     }
   };
+  splitters.words.formatter = formatters.numbered('word',' ');
+  splitters.chars.formatter = formatters.numbered('char','');
+  splitters.lines.formatter = formatters.numbered('line','');
 
   $.ender({
     littering: function(splitter, formatter) {
       if (!splitter) splitter = 'chars';
-      splitters[splitter](this);
-
+      if (typeof(splitter) != 'function') {
+        splitter = splitters[splitter];
+      }
+      if (typeof(formatter) == 'function') {
+        splitter(this, formatter);
+      } else {
+        splitter(this, splitter.formatter);
+      }
       return this;
     }
   },true);
+
+  $.ender({
+    splitters: splitters,
+    formatters: formatters
+  });
 }(ender);
