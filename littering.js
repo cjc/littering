9dcb01e99a134f70ffdabebe02cffc09073d7360
Cameron <cjc@cjc.id.au>
2011-05-16 23:30:14 +1000

    Add initial version with partial Lettering.js functionality working with Ender, examples ripped directly from Lettering.js

---
+++ littering.js
@@ -0,0 +1,17 @@
+!function($) {
+  var injector = function(t,splitter,klass,after) {
+      var a = $(t).text().split('');
+      if (a.length) {
+        var inject = '';
+        $(a).each(function(item,i) {
+          inject += '<span class="'+klass+(i+1)+'">'+item+'</span>'+after;
+        });
+        $(t).empty().append(inject);
+      }
+  };
+  $.ender({
+    littering: function() {
+      this.each(function(item){injector(item,'','char','')});
+    }
+  },true);
+}(ender);
