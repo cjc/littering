502be15a6fadd6646aaf65574bc5cb3bdc465d16
Cameron <cjc@cjc.id.au>
2011-05-17 00:09:14 +1000

    Pinch more code from Lettering.js to get the example working

--- littering.js
+++ littering.js
@@ -1,6 +1,6 @@
 !function($) {
   var injector = function(t,splitter,klass,after) {
-      var a = $(t).text().split('');
+      var a = $(t).text().split(splitter);
       if (a.length) {
         var inject = '';
         $(a).each(function(item,i) {
@@ -10,8 +10,18 @@
       }
   };
   $.ender({
-    littering: function() {
-      this.each(function(item){injector(item,'','char','')});
+    littering: function(type) {
+      if (type === 'words') {
+        this.each(function(item){injector(item,' ','word',' ')});
+      } else if (type === 'lines') {
+        var r = "eefec303079ad17405c889e092e105b0";
+        this.each(function(item){
+          $(item).children("br").after(r).remove();
+          injector(item,r,'line','');
+        });
+      } else {
+        this.each(function(item){injector(item,'','char','')});
+      }
       return this;
     }
   },true);
